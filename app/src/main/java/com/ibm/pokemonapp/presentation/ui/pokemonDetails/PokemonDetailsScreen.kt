a37889775c64b638ce6bedf0966b599f56b8cde2
Abdelnga <abdelilah.ngadi@idemia.com>
2024-01-28 11:34:01 +0100

    Add Animation to LinearProgressIndicator

--- app/src/main/java/com/ibm/pokemonapp/presentation/ui/pokemonDetails/PokemonDetailsScreen.kt
+++ app/src/main/java/com/ibm/pokemonapp/presentation/ui/pokemonDetails/PokemonDetailsScreen.kt
@@ -1,5 +1,7 @@
 package com.ibm.pokemonapp.presentation.ui.pokemonDetails
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -23,8 +25,12 @@ import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.produceState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -49,6 +55,7 @@ import com.ibm.pokemonapp.presentation.ui.theme.Red
 import com.ibm.pokemonapp.presentation.ui.theme.Roboto
 import com.ibm.pokemonapp.utils.Consts.DREAM_WORLD_IMAGES_URL
 import com.ibm.pokemonapp.utils.InfoPair
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.first
 
 @Composable
@@ -329,13 +336,24 @@ fun PokemonInfoPair(infoPair: InfoPair, modifier: Modifier) {
 fun StatesProgressBar(
     globalState: Float
 ) {
+    var animationPlayed by remember {
+        mutableStateOf(false)
+    }
+    val currentPercentage = animateFloatAsState(
+        targetValue =
+        if (animationPlayed) globalState else 0f,
+        animationSpec = tween(durationMillis = 1000), label = ""
+    )
+    LaunchedEffect(key1 = true) {
+        animationPlayed = true
+    }
     Box(
         modifier = Modifier
             .fillMaxWidth()
             .padding(16.dp)
     ) {
         LinearProgressIndicator(
-            progress = globalState,
+            progress = currentPercentage.value,
             color = MaterialTheme.colorScheme.secondary,
             trackColor = MaterialTheme.colorScheme.primary,
             modifier = Modifier
